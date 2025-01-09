a390d0268df25920801b06e5c80ea91de4d98540
sachiniwathudura <sachiniwathudura@gmail.com>
2025-01-09 19:30:44 +0530

    add - day 8 python lesson

---
+++ test/test.py
@@ -0,0 +1,11 @@
+# import pdb
+def multiplication(a,b):
+    answer = a * b
+    return answer
+# pdb.set_trace()
+breakpoint()
+x = input("enter first number : ")
+y = input("enter second number")
+
+mul = multiplication(x,y)
+print(mul)
